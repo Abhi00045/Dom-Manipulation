614d82e169d8797fea727e10f08fef120a863130
Abhi00045 <128639147+Abhi00045@users.noreply.github.com>
2024-05-21 15:17:27 +0530

    saved

---
+++ index.js
@@ -0,0 +1,15 @@
+
+let divElement = document.getElementById("news");
+
+
+  
+divElement.style.backgroundColor = "yellow"; 
+divElement.style.margin = "20px";           
+divElement.style.padding = "10px";                
+divElement.style.height = "200px";          
+divElement.style.width = "300px";
+
+divElement.style.color = "blue";
+divElement.style.fontSize = "18px";         
+divElement.style.fontWeight = "bold"; 
+
